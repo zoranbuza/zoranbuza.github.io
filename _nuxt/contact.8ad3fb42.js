7974c9523e6f5450c7517a306ba810106035ce5d
zoranbuza <62284990+zoranbuza@users.noreply.github.com>
2023-09-23 10:02:36 +0200

    my first commit

---
+++ _nuxt/contact.8ad3fb42.js
@@ -0,0 +1,1 @@
+import{_ as c,o as t,c as e}from"./entry.b588904d.js";const o={},n={class:"uk-container"};function s(a,r){return t(),e("div",n," Kontakt ")}const f=c(o,[["render",s]]);export{f as default};
---
+++ _nuxt/practices.68703c67.js
@@ -0,0 +1,1 @@
+import{_ as c,o as e,c as t}from"./entry.b588904d.js";const o={},s={class:"uk-container"};function n(r,a){return e(),t("div",s)}const i=c(o,[["render",n]]);export{i as default};
